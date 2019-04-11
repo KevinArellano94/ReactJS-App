6c2d75ac01a4648e94e3c28af375b0ecb6de5d78
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-11 14:16:20 -0400

    .
    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function HomePage(props) {
   return (
     <div className="HomePage">
       <h2>{props.name}</h2>
-      <h2>T-rex Scans 2.1</h2>
+      <h2>T-rex Scans 3.1</h2>
       <p>A multilingual scanlation group.</p>
       <embed src="https://img.shields.io/discord/518583314710528005.svg?style=badge" />
     </div>
