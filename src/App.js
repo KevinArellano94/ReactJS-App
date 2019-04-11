a6423214cea5ee90ed5658aeb3a1cc15450062f1
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-04-11 14:42:03 -0400

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 //import ReactDOM from "react-dom";
 import {
   BrowserRouter,
@@ -51,11 +51,6 @@ function NavigationBar() {
         <Route render={NoMatchPage} />
       </Switch>
 
-      <ul className="nav navbar-nav">
-        <li>
-          <NetlifyAuth />
-        </li>
-      </ul>
       <searchIcon toggleSearch={"toggleSearch"} />
       <ul className="nav navbar-nav">
         <li>
@@ -66,10 +61,77 @@ function NavigationBar() {
   );
 }
 
+class SlackMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, text: null, error: null, success: false };
+  }
+  handleText = e => {
+    this.setState({ text: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    fetch("/.netlify/functions/slack", {
+      method: "POST",
+      body: JSON.stringify({
+        text: this.state.text
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(err => {
+            throw err;
+          });
+        }
+      })
+      .then(() =>
+        this.setState({
+          loading: false,
+          text: null,
+          success: true,
+          error: null
+        })
+      )
+      .catch(err =>
+        this.setState({ loading: false, success: false, error: err.toString() })
+      );
+  };
+  render() {
+    const { loading, text, error, success } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {error && (
+          <p>
+            <strong>Error sending message: {error}</strong>
+          </p>
+        )}
+        {success && (
+          <p>
+            <strong>Done! Message sent to Slack</strong>
+          </p>
+        )}
+        <p>
+          <label>
+            Your Message: <br />
+            <textarea onChange={this.handleText} value={text} />
+          </label>
+        </p>
+        <p>
+          <button type="submit" disabled={loading}>
+            {loading ? "Sending Slack Message..." : "Send a Slack Message"}
+          </button>
+        </p>
+      </form>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <SlackMessage />
       <Footer />
     </div>
   );
